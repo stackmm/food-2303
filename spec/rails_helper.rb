99f2b0e6500b31e03fb2c36acc65dc7055b367e8
Steven Stackhouse <stackmm@gmail.com>
2023-07-21 09:16:41 -0600

    add gems and create spec files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,11 +71,3 @@ Shoulda::Matchers.configure do |config|
      with.library :rails
   end
 end
-
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock
-  config.filter_sensitive_data('<X-Api-Key>') { ENV["X-Api-Key"] }
-  config.default_cassette_options = { re_record_interval: 7.days }
-  config.configure_rspec_metadata!
-end
