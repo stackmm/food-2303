b8681b039c9b6d4c7e157b6a8103cf4d8b16dff4
Steven Stackhouse <stackmm@gmail.com>
2023-07-21 10:24:12 -0600

    added facade for num search results

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -2,8 +2,14 @@ class FoodFacade
   def self.search_food(food)
     json = FoodService.get_food(food)
 
-    json[:foods].map do |food_data|
+    json[:foods].take(10).map do |food_data|
       Food.new(food_data)
     end
+  end
+
+  def self.total_results(food)
+    json = FoodService.get_food(food)
+
+    json[:totalHits]
   end
 end
--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe FoodFacade do
   it 'returns a list of foods by search query' do
-    bananas = FoodFacade.search_food('banana')
+    potatoes = FoodFacade.search_food('sweet potatoes')
 
-    expect(bananas).to be_an(Array)
-    expect(bananas.first).to be_a(Food)
-    expect(bananas.first.gtinupc).to eq('016459200441')
-    expect(bananas.first.description).to eq('BANANA')
-    expect(bananas.first.brand_owner).to eq('Wonder Natural Foods Corp')
+    expect(potatoes).to be_an(Array)
+    expect(potatoes.first).to be_a(Food)
+    expect(potatoes.first.gtinupc).to eq('8901020020844')
+    expect(potatoes.first.description).to eq('SWEET POTATOES')
+    expect(potatoes.first.brand_owner).to eq('NOT A BRANDED ITEM')
+  end
+
+  it 'returns the total number of results' do
+    num_results = FoodFacade.total_results('sweet potatoes')
+
+    expect(num_results).to eq(55579)
   end
 end
